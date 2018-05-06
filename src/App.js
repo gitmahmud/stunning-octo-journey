ecd025e0a3d2c16f1f573cf4e73f3b724e891ba1
Mahmud Rahman <mahmud@Mahmuds-MacBook-Pro.local>
2018-05-06 18:13:34 +0800

    added reacttable , csv data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,118 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
+
 
 class App extends Component {
+
   render() {
+   
+    const eng2pin = [
+    {
+      "#" : 1,
+      "eng" : "Do you speak English?",
+      "pin" : "Nǐ huì shuō yīngyǔ ma"
+    },
+    {
+      "#" : 2,
+      "eng" : "My name is",
+      "pin" : "wǒ jiào"
+    },
+    {
+      "#" : 3,
+      "eng" : "Taxi",
+      "pin" : "chūzū chē"
+    },
+    {
+      "#" : 4,
+      "eng" : "Goodbye",
+      "pin" : "Zàijiàn"
+    },
+    {
+      "#" : 5,
+      "eng" : "Please take me to the airport",
+      "pin" : "qǐng sòng wǒ dào jīchǎng"
+    },
+    {
+      "#" : 6,
+      "eng" : "How much",
+      "pin" : "Duōshǎo"
+    },
+    {
+      "#" : 7,
+      "eng" : "I/me",
+      "pin" : "Wǒ"
+    },
+    {
+      "#" : 8,
+      "eng" : "Where is the restroom",
+      "pin" : "xǐshǒujiān zài nǎli"
+    },
+    {
+      "#" : 9,
+      "eng" : "How are you?",
+      "pin" : "Nǐ hǎo ma?"
+    },
+    {
+      "#" : 10,
+      "eng" : "Eat",
+      "pin" : "Chī"
+    },
+    {
+      "#" : 11,
+      "eng" : "Thank you",
+      "pin" : "Xièxiè"
+    },
+    {
+      "#" : 12,
+      "eng" : "I am sorry",
+      "pin" : "Duìbùqǐ"
+    },
+    {
+      "#" : 13,
+      "eng" : "Phone number",
+      "pin" : "diànhuà hàomǎ"
+    },
+    {
+      "#" : 14,
+      "eng" : "Yes",
+      "pin" : "shì"
+    },
+    {
+      "#" : 15,
+      "eng" : "No",
+      "pin" : "bú shì"
+    }
+
+    ];
+    
     return (
       <div className="App">
-        <h1>Hi</h1>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+         <h1>Conversational Language Learning</h1>
+         <a href="http://www.ctcfl.ox.ac.uk/Pinyin_Notes.htm" target="_blank">How to read pinyin</a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+        <ReactTable
+         data={eng2pin}
+         columns={[{
+        Header: '#',
+        accessor: '#' // String-based value accessors!
+      }, {
+        Header: 'English',
+        accessor: 'eng',
+        
+      }, {
+        Header: 'PinYin',
+        accessor: 'pin'
+      }]}
+         defaultPageSize={10}
+         className="-striped -highlight"
+        />
+        </div>
+
+      
       </div>
     );
   }
